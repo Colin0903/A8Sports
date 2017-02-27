8ba0fe97e6804fc5345d48091bfe1863cf37fbec
Czeal <65420220@qq.com>
2017-02-27 16:55:33 +0800

    分包

---
+++ app/src/main/java/com/company/a8sports/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.company.a8sports.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.company.a8sports.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
