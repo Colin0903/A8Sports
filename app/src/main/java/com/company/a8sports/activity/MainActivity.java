0d83866221abd5b430aeba3f80c12e9be7973e6b
zhaochuan <email@example.com>
2017-02-27 16:11:12 +0800

    upload

--- app/src/main/java/com/company/a8sports/activity/MainActivity.java
+++ app/src/main/java/com/company/a8sports/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.company.a8sports;
+package com.company.a8sports.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.company.a8sports.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/company/a8sports/config/apiService.java
@@ -0,0 +1,9 @@
+package com.company.a8sports.config;
+
+/**
+ * Created by zc on 2017/2/27.
+ */
+
+public class apiService {
+
+}
